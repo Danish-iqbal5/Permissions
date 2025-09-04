ed4cea235be79805961ecc37116c98f9dccf980a
Danish-noob <157475336+Danish-noob@users.noreply.github.com>
2025-09-04 12:20:23 +0500

    Added Swagger

--- Permissions/urls.py
+++ Permissions/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from . import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="API documentation for your project",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('set-password/<int:user_id>/', views.SetPasswordView.as_view(), name='set-password'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('verify-otp/', views.VerifyOTPView.as_view(), name='verify-otp'),
     path('admin-dashboard/', views.AdminDashboardRequestApprovalView.as_view(), name='admin-requests'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
